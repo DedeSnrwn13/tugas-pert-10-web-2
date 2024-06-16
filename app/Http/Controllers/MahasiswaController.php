97e3ff49384b888fdb02ca814bbc35073292a927
DedeSnrwn13 <sunarwandede13@gmail.com>
2024-06-16 14:00:24 +0700

    feat: mahasiswa controller

---
+++ app/Http/Controllers/MahasiswaController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Mahasiswa;
+use Illuminate\Http\Request;
+
+class MahasiswaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $mahasiswas = Mahasiswa::all();
+
+        return view('mahasiswa.index', compact('mahasiswas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('mahasiswa.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        Mahasiswa::create($request->all());
+
+        return redirect()->route('mahasiswa.index')->with('success', 'Student created successfully');
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Mahasiswa $mahasiswa)
+    {
+        return view('mahasiswa.show', compact('mahasiswa'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Mahasiswa $mahasiswa)
+    {
+        return view('mahasiswa.edit', compact('mahasiswa'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Mahasiswa $mahasiswa)
+    {
+       $mahasiswa->update($request->all());
+
+       return redirect()->route('mahasiswa.index')->with('success', 'Student updated successfully');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Mahasiswa $mahasiswa)
+    {
+        $mahasiswa->delete();
+
+        return redirect()->route('mahasiswa.index')->with('success', 'Student deleted successfully');
+    }
+}
